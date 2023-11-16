e42ed0628a4afb6a8e6d43ecaa77f530387f376b
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-16 10:36:20 +0100

    Understanding main method in Java

--- src/com/dgmf/JavaTestClass.java
+++ src/com/dgmf/JavaTestClass.java
@@ -3,5 +3,8 @@ package com.dgmf;
 public class JavaTestClass {
     public static void main(String[] args) {
         System.out.println("Programming in Java.");
+        System.out.println("This is another Statement");
+        System.out.println("Values of \"args\" are : " + "not provided");
+        System.out.println("Values of \"args\" are : " + args);
     }
 }
