de45ad12a793ea5d67c9597a2c3b7859acafbee3
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-16 10:50:47 +0100

    Arithmetic Operators in Java

---
+++ src/com/dgmf/arithmeticoperators/ArithmeticOperators.java
@@ -0,0 +1,19 @@
+package com.dgmf.arithmeticoperators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int number1 = 15;
+        int number2 = 12;
+        int sum = number1 + number2;
+        int difference = number1 - number2;
+        int product = number1 * number2;
+        int integerDivision = number1 / number2;
+        int modulo = number1 % number2;
+
+        System.out.println("The sum is : " + sum);
+        System.out.println("The difference is : " + difference);
+        System.out.println("The product is : " + product);
+        System.out.println("The integer division is : " + integerDivision);
+        System.out.println("The modulo is : " + modulo);
+    }
+}
